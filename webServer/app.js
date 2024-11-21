bee83d52c37893f50a5c9b24b1c0a48f100c31c1
DiegoAntonio102 <diegoantoniomartinez2002@gmail.com>
2024-11-20 22:04:30 -0600

    Add files via upload

---
+++ webServer/app.js
@@ -0,0 +1,11 @@
+const http = require('http');
+                                    //request, response
+const server = http.createServer((req, res) => {
+    console.log(req.url);
+    res.writeHead(201);
+    res.write('Hello Ana!');
+    res.end();
+});
+
+server.listen(3000, '127.0.0.1');
+console.log('Servidor web iniciado en http://127.0.0.1:3000')
