b7e5f2beba9fff4cfb087a331ed3c29e59270272
DiegoAntonio102 <diegoantoniomartinez2002@gmail.com>
2024-11-20 22:04:04 -0600

    Add files via upload

---
+++ holaMundo/hola.js
@@ -0,0 +1,10 @@
+const mensaje = "Hola Mundo"
+const otromensaje = "Este es otro mensaje"
+
+console.log(mensaje)
+
+if(mensaje){
+    console.log("Se imprimio el mensaje")
+}
+
+module.exports = {otromensaje}
